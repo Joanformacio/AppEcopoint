348c254bc5454ef2ebac7bfa86dd103beeadff5b
joanformacio <joanformacio@outlook.es>
2021-11-08 19:22:26 +0100

    update serviceLocations

--- service/ServiceMaps.js
+++ service/ServiceMaps.js
@@ -1,3 +1,29 @@
 import axios from 'axios'
 
+const baseURL = "http://192.168.18.57:8080/api/"
 
+export const getallLocations= async()=>{
+    try {
+        const dataLocations = await axios.post(baseURL + "login", data, config)
+
+        const locations = dataLocations.data
+
+        return locations
+
+    } catch (error) {
+        console.error(" when get all locations", error)
+    }
+}
+
+export const saveImageLocation = async(Image){
+    try {
+        const res = await axios.post(baseURL+ "urlsaveimage",Image)
+    } catch (error) {
+        console.error(" when save image", error)
+    }
+    
+    if (res!=null){
+        console.log(res.data)
+        return res.data
+    } 
+}
